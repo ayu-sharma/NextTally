8a8f6038d5d8a51686eceba7a3682eff6b75f718
AkshatJha21 <akshatj2134@gmail.com>
2024-10-07 11:54:03 +0530

    manager GET branch details done

---
+++ app/api/manager/branch/route.ts
@@ -0,0 +1,75 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "../../../../lib/db";
+
+export async function GET(request: NextRequest) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const managerId = parseInt(searchParams.get('managerId'), 10);
+
+        if (!managerId || isNaN(managerId)) {
+            return NextResponse.json({
+                error: 'Manager ID is required and must be a valid integer'
+            }, {
+                status: 400
+            });
+        }
+
+        const manager = await db.manager.findUnique({
+            where: {
+                id: managerId
+            },
+            include: {
+                branch: true
+            }
+        });
+
+        if (!manager || !manager.branch) {
+            return NextResponse.json({
+                error: "Manager not found or not associated with any branch"
+            }, {
+                status: 400
+            });
+        }
+
+        const branchId = manager.branch.id;
+
+        const branchDetails = await db.branch.findUnique({
+            where: {
+                id: branchId
+            },
+            include: {
+                movies: {
+                    include: {
+                        seatCategories: {
+                            include: {
+                                seatBookings: true
+                            }
+                        }
+                    }
+                },
+                manager: true
+            }
+        });
+
+        if (!branchDetails) {
+            return NextResponse.json({
+                error: "Branch details not found"
+            }, {
+                status: 400
+            });
+        }
+
+        return NextResponse.json({
+            branchDetails
+        }, {
+            status: 200
+        });
+    } catch (error) {
+        console.error("Error while fetching branch details: ", error);
+        return NextResponse.json({
+            error: "Error while fetching branch details"
+        }, {
+            status: 500
+        });
+    }
+}
