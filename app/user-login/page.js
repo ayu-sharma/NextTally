aaa51eb66cb3c515251be46468a89e95d022b248
ayu-sharma <contactayusharma@gmail.com>
2024-10-18 06:04:39 +0530

    remainig with desing errors

--- app/user-login/page.js
+++ app/user-login/page.js
@@ -1,47 +1,59 @@
-import react from 'react';
-import Button from '../../components/ui/ButtonCmp';
-import Image from 'next/image';
-import googlelogo from '../../public/images/googlelogo.svg';
-import close from '../../public/images/modalclose.svg'
-import logol from '../../public/images/logol.svg';
+import react from "react";
+import Button from "../../components/ui/ButtonCmp";
+import Image from "next/image";
+import googlelogo from "../../public/images/googlelogo.svg";
+import close from "../../public/images/modalclose.svg";
+import logol from "../../public/images/logol.svg";
 // import { Image } from 'next/image';
 export default function Signin_admin({ onClose }) {
-    return (
-        <>
-            <div className='w-full overflow-y-auto'>
-                <div className='flex flex-col items-center md:hidden pt-4'>
-                    <Image src={logol} alt='NextTally Logo' height={36} />
-                </div>
-                <div className='flex flex-col w-full'>
-                    <div className='flex-1 items-start flex flex-row w-full'>
-                        <div className='hidden md:flex bg-gradient-to-b from-[#185FF6] to-[#1B45A6] h-[46rem] max-w-xl w-full flex-col pt-20 pl-10'>
-                            <div className='text-white font-bold text-2xl'>
-                                The Simplest way to manage <br /> your revenue
-                            </div>
-                        </div>
-                        <div className='flex flex-col justify-center w-full md:pt-24 pt-16 md:pb-10 md:px-4 px-4 max-w-md mx-auto'>
-                            <div className='text-2xl font-bold'>
-                                Access Your Dashboard
-                            </div>
-                            <div className='text-black text-xs'>
-                                Enter you credentials to access your account
-                            </div>
-                            <div className='pt-6 w-full flex flex-col'>
-                                <input type='email' placeholder='Email Address..' className='border-2 border-gray-300 rounded-md py-2.5 px-6 w-full mt-1 text-xs' />
-
-                                <input type='password' placeholder='Password' className='border-2 border-gray-300 rounded-md py-2.5 px-6 w-full text-xs mt-4' />
-
-                            </div>
-                            <div className='w-full'>
-                                <Button className='rounded-md text-sm py-2.5 px-4 text-white mt-5 font-bold hover:opacity-90 max-w-md mx-auto w-full' btnName="Login" />
-                            </div>
-                            <div className='text-black font-bold text-xs mt-1'>
-                                Don&apos;t have an account yet? No problem! <a className='text-blue-700' href='#'>Create one now</a>.
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="w-full overflow-y-auto">
+        <div className="flex flex-col w-full">
+          <div className="flex-1 items-center flex flex-row w-full">
+            <div className="hidden md:flex bg-gradient-to-b from-[#185FF6] to-[#1B45A6] h-screen max-w-lg w-full flex-col pt-20 pl-10">
+              <div className="font-inter font-semibold md:text-[2rem] leading-[1.2] antialiased text-primary -tracking-[0.019em] md:-tracking-[0.021em] text-white ">
+                Effortless Revenue Management <br /> Made Simple
+              </div>
             </div>
-        </>
-    )
+            <div className="flex flex-col relative items-start max-w-md mx-auto justify-center w-full h-full py-12 px-4">
+              <div className="font-inter font-medium text-2xl md:text-[2rem] leading-[1.2] antialiased text-primary -tracking-[0.019em] md:-tracking-[0.021em] ">
+                Access Theater Operations
+              </div>
+              <div className="text-black text-xs">
+                Enter you credentials to access your account
+              </div>
+              <div className="mt-10 w-full flex flex-col">
+                <input
+                  type="email"
+                  placeholder="Email Address.."
+                  className="w-full px-6 rounded-lg antialiased text-primary font-normal focus:outline-none py-3 border border-slate-300 focus:border-studio-gradient-start/60 focus:ring-1 focus:ring-studio-gradient-start/60 mb-4 placeholder:font-[350]"
+                />
+
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full px-6 rounded-lg antialiased text-primary font-normal focus:outline-none py-3 border border-slate-300 focus:border-studio-gradient-start/60 focus:ring-1 focus:ring-studio-gradient-start/60 mb-4 placeholder:font-[350]"
+                />
+              </div>
+              <div className="w-full">
+                <Button
+                  className="cursor-pointer studio-primary-gradient font-inter text-sm md:text-base -tracking-[0.006em] md:-tracking-[0.011em] text-white font-medium antialiased rounded-lg py-2.5 px-6 md:px-8 flex items-center justify-center group w-full"
+                  btnName="Login"
+                />
+              </div>
+              <div className="text-black font-bold text-xs mt-1">
+                Don&apos;t have an account yet? No problem!{" "}
+                <a className="text-blue-700" href="#">
+                  Create one now
+                </a>
+                .
+              </div>
+              </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
