00db12a6f7fd6b1e3abc006eccc17b698d18667f
ayu-sharma <contactayusharma@gmail.com>
2024-10-23 20:48:26 +0530

    final commit before diwali

--- app/admin-signup/page.js
+++ app/admin-signup/page.js
@@ -1,10 +1,22 @@
-import React from "react";
+"use client"
+import React, {use, useState, useEffect} from "react";
 import Signup_cmp from "../../components/Singup_cmp";
 import Link from "next/link";
 import logol from "../../public/images/logol.svg";
 import Image from "next/image";
 import signuplogo from "../../public/images/signup_logo.svg";
-export default function Signin_admin({ onClose }) {
+import Branch_cmp from "../../components/Branch_cmp"
+import Assign_mngr from "../../components/Assign_mngr"
+export default function Signin_admin() {
+  const [page, setPage ] = useState(1)
+
+  const renderPage = (e) => { 
+    console.log("clicked")
+  if(page < 3){
+    setPage(page + 1)
+  }
+}
+
   return (
     <>
       <div className="w-full overflow-y-auto">
@@ -13,16 +25,26 @@ export default function Signin_admin({ onClose }) {
             <div className="home-navbar-color h-screen w-full max-w-xs">
               <div className="py-5 pl-6">
                 <Image src={logol} alt="NextTally Logo" height={38} />
-                {/* <div className="flex flex-col"> */}
-                  <div className="flex flex-row items-center pt-10 gap-4">
-                  <Image className="bg-white p-3 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
+                <div className="flex flex-col mt-10 gap-7">
+                  <div className="flex flex-row items-center gap-4 cursor-pointer">
+                  <Image className="bg-white p-2 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
                   <h1 className="">Admin Signup</h1>
-                {/* </div> */}
+                </div>
+                <div className="flex flex-row items-center gap-4 cursor-pointer">
+                  <Image className="bg-white p-2 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
+                  <h1 className="">Branch Details</h1>
+                </div>
+                <div className="flex flex-row items-center gap-4 cursor-pointer">
+                  <Image className="bg-white p-2 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
+                  <h1 className="">Assign Manager</h1>
+                </div>
                 </div>
               </div>
             </div>
             <div className="flex flex-col w-full md:px-4 px-4 max-w-md mx-auto">
-              <Signup_cmp />
+              {page === 1 && <Signup_cmp  renderPage={renderPage}/>}
+              {page === 2 && <Branch_cmp renderPage={renderPage}/>}
+              {page === 3 && <Assign_mngr renderPage={renderPage}/>}
             </div>
           </div>
         </div>
--- components/ui/ButtonCmp.js
+++ components/ui/ButtonCmp.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Button } from '@headlessui/react'
 
-export default function ButtonCmp({ btnName, className, style }) {
+export default function ButtonCmp({ btnName, className, style, onClick,  }) {
   return (
     <div>
-            <Button style={{ backgroundColor: 'black', ...style }} className={`bg-gradient-to-r from-[#185FF6] to-[#1B45A6] via-[#185FF6] bg-[18%_91%] ${className}`}>{btnName}</Button>
+            <Button style={{ backgroundColor: 'black', ...style }} className={`bg-gradient-to-r from-[#185FF6] to-[#1B45A6] via-[#185FF6] bg-[18%_91%] ${className}`} onClick={onClick}>{btnName}</Button>
         </div>
   )
 }
