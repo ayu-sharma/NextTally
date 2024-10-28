9772243c4a421d3d479b79fa09e0a662d61fceeb
ayu-sharma <contactayusharma@gmail.com>
2024-10-28 17:34:03 +0530

    updated the code

--- app/admin-signup/page.js
+++ app/admin-signup/page.js
@@ -17,24 +17,26 @@ export default function Signin_admin() {
   }
 }
 
+
+
   return (
     <>
       <div className="w-full overflow-y-auto">
         <div className="flex flex-col w-full">
           <div className="flex-1 flex items-center flex-row w-full">
             <div className="home-navbar-color h-screen w-full max-w-xs">
-              <div className="py-5 pl-6">
-                <Image src={logol} alt="NextTally Logo" height={38} />
-                <div className="flex flex-col mt-10 gap-7">
-                  <div className="flex flex-row items-center gap-4 cursor-pointer">
+              <div className={`py-4`}>
+                <Image className="pl-6" src={logol} alt="NextTally Logo" height={38} />
+                <div className="flex flex-col mt-10 gap-3">
+                  <div className={`flex flex-row items-center gap-3 cursor-pointer ${page === 1 ? "bg-gradient-to-b from-[#185FF6] to-[#1B45A6] py-3 px-6 text-white" : "bg-none px-6 py-3"}`}>
                   <Image className="bg-white p-2 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
                   <h1 className="">Admin Signup</h1>
                 </div>
-                <div className="flex flex-row items-center gap-4 cursor-pointer">
+                <div className={`flex flex-row items-center gap-3 cursor-pointer ${page === 2 ? "bg-gradient-to-b from-[#185FF6] to-[#1B45A6] py-3 px-6 text-white" : "bg-none px-6 py-3"}`}>
                   <Image className="bg-white p-2 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
                   <h1 className="">Branch Details</h1>
                 </div>
-                <div className="flex flex-row items-center gap-4 cursor-pointer">
+                <div className={`flex flex-row items-center gap-3 cursor-pointer ${page === 3 ? "bg-gradient-to-b from-[#185FF6] to-[#1B45A6] py-3 px-6 text-white" : "bg-none px-6 py-3"}`}>
                   <Image className="bg-white p-2 rounded-md" src={signuplogo} alt="Signup Logo" height={30} width={30}/>
                   <h1 className="">Assign Manager</h1>
                 </div>
